320fc279b7e42fbca6859b9c5d6e6fed29ddc987
monolok <antoinebertin@me.com>
2015-01-23 21:51:17 +0100

    Set up DB Employee

--- db/migrate/20150123203934_create_employees.rb
+++ db/migrate/20150123203934_create_employees.rb
@@ -1,6 +1,10 @@
 class CreateEmployees < ActiveRecord::Migration
   def change
     create_table :employees do |t|
+    	t.string :name
+    	t.string :position
+    	t.integer :result
+
 
       t.timestamps null: false
     end
