83b8c6408cfbe5b871f17352af5f1d1306ce041d
monolok <antoinebertin@me.com>
2015-02-13 00:31:32 +0100

    code commented for limitation based on plan subscribed

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,12 @@
 class Employee < ActiveRecord::Base
 	belongs_to :user
 	has_many :scores
+
+ #  	validate :thing_count_within_limit, :on => :create
+
+	# def thing_count_within_limit
+	# 	if self.user.things(:reload).count >= 5
+	# 	  errors.add(:base, "Exceeded thing limit")
+	# 	end
+	# end
 end
--- app/models/reviewer.rb
+++ app/models/reviewer.rb
@@ -5,4 +5,13 @@ class Reviewer < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 	belongs_to :user
 	has_many :scores
+
+ #  	validate :thing_count_within_limit, :on => :create
+
+	# def thing_count_within_limit
+	# 	if self.user.things(:reload).count >= 5
+	# 	  errors.add(:base, "Exceeded thing limit")
+	# 	end
+	# end
+	
 end
